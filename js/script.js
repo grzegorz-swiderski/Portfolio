5dda050daf46a26b4fb17ba3b5f4559b3f3eef68
Grzegorz Swiderski <grzegorzswiderski1988@gmail.com>
2017-10-05 03:54:21 +0200

    all Portfolio

---
+++ js/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+       
+///scroll
+    
+    $('a[href^="#"]').on('click', function(event) {
+		var target = $( $(this).attr('href') );
+	
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 1000);
+		}
+	});
+    
+///button-menu
+    
+    $(".fa-arrow-up").click(function(){
+		$("html,body").animate({scrollTop: 0},500);
+	});
+	
+	$(document).scroll(function(){
+		var position=$(this).scrollTop();
+		
+		if((position<250) || (position>3000)){
+			$(".fa-arrow-up").fadeOut();
+		} else {
+			$(".fa-arrow-up").fadeIn();
+		}
+	});
+    
+///slick
+
+    $('.center').slick({
+        slidesToShow: 2,
+        prevArrow: $('.prev-community'),
+        nextArrow: $('.next-community'),
+        responsive: [
+            {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    
+///technology
+    
+    
+    var $grid = $( '#tp-grid' ),
+        $name = $( '#name' ),
+        $close = $( '#close' ),
+        $loader = $( '<div class="loader"><i></i><i></i><i></i><i></i><i></i><i></i><span>Loading...</span></div>' ).insertBefore( $grid ),
+        stapel = $grid.stapel( {
+				onLoad : function() {
+				$loader.remove();
+				},
+				onBeforeOpen : function( pileName ) {
+				    $name.html( pileName );
+				},
+				onAfterOpen : function( pileName ) {
+				    $close.show();
+						}
+					} );
+
+				$close.on( 'click', function() {
+					$close.hide();
+					$name.empty();
+					stapel.closePile();  
+            });
+});
